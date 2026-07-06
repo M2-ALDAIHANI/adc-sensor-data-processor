35284b46f8ae61cb9fb20a0dd9c485ef3c4de8d8
M2-ALDAIHANI <majed2.aldaihani@live.uwe.ac.uk>
2026-07-06 15:59:18 +0300

    Connect program flow and document usage

--- main.c
+++ main.c
@@ -1,9 +1,56 @@
+#include "adc.h"
+#include "io.h"
+
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[]) {
-    (void)argc;
-    (void)argv;
+    ADCHeader header;
+    ADCSample *samples;
+    ChannelReport reports[ADC_CHANNEL_COUNT];
+    SequenceGap gaps[20];
+    size_t gap_count;
 
-    printf("ADC Sensor Data Processor\n");
+    if (argc != 2) {
+        printf("Usage: %s adc_sensor_log.bin\n", argv[0]);
+        return 1;
+    }
+
+    printf("Reading ADC file: %s\n", argv[1]);
+
+    samples = io_read_adc_file(argv[1], &header);
+
+    if (samples == NULL) {
+        return 1;
+    }
+
+    adc_compute_channel_reports(samples, header.record_count, reports);
+
+    gap_count = adc_check_sequence_gaps(samples,
+                                        header.record_count,
+                                        gaps,
+                                        20);
+
+    if (!io_write_results_file("results.txt",
+                               &header,
+                               reports,
+                               gaps,
+                               gap_count)) {
+        free(samples);
+        return 1;
+    }
+
+    if (!io_write_fault_report_file("fault_report.txt",
+                                    samples,
+                                    header.record_count)) {
+        free(samples);
+        return 1;
+    }
+
+    free(samples);
+
+    printf("Analysis complete.\n");
+    printf("Created results.txt and fault_report.txt.\n");
+
     return 0;
 }
